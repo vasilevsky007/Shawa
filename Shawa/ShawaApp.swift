e375e8940a98e0d25ee20a5d0f6dd0020455256c
alex <alex.vasilevski007@gmail.com>
2023-03-10 15:37:32 +0300

    Initial Commit

---
+++ Shawa/ShawaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShawaApp.swift
+//  Shawa
+//
+//  Created by Alex on 10.03.23.
+//
+
+import SwiftUI
+
+@main
+struct ShawaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AuthoriseView()
+        }
+    }
+}
---
+++ Shawa/View/AuthoriseView.swift
@@ -0,0 +1,26 @@
+//
+//  AuthoriseView.swift
+//  Shawa
+//
+//  Created by Alex on 10.03.23.
+//
+
+import SwiftUI
+
+struct AuthoriseView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct AuthoriseView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthoriseView()
+    }
+}
