528a157c3bae5fe13720281aa059d7a26f58400b
alex <alex.vasilevski007@gmail.com>
2024-03-30 13:12:53 +0300

    refactor: continue changing Firebase API. done with Auth

---
+++ Shawa/Model/Authentication/Authentication.swift
@@ -0,0 +1,26 @@
+//
+//  Authentication.swift
+//  Shawa
+//
+//  Created by Alex on 30.03.24.
+//
+
+import Foundation
+
+protocol Authentication {
+    var uid: String? { get }
+    var name: String? { get }
+    var email: String? { get }
+    var phone: String? { get }
+    var isAuthenticated: Bool { get }
+    var isAuthenticating: Bool { get }
+    var currentError: Error? { get }
+    
+    mutating func register(withEmail email: String, password: String) async
+    mutating func login(withEmail email: String, password: String) async
+    mutating func logout()
+    mutating func deleteAccount() async
+    mutating func updateEmail(to email: String) async
+    mutating func updateName(to name: String) async
+    mutating func clearError()
+}
---
+++ Shawa/Model/Authentication/FirebaseAuthentication.swift
@@ -0,0 +1,131 @@
+//
+//  FirebaseAuthentication.swift
+//  Shawa
+//
+//  Created by Alex on 29.03.24.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+
+struct FirebaseAuthentication: Authentication {
+    private var currentUser: User?
+    
+    var uid: String? {
+        currentUser?.uid
+    }
+    
+    var name: String? {
+        currentUser?.displayName
+    }
+        
+    var email: String? {
+        currentUser?.email
+    }
+        
+    var phone: String? {
+        currentUser?.phoneNumber
+    }
+    
+    var isAuthenticated: Bool {
+        currentUser != nil
+    }
+    
+    private (set) var isAuthenticating: Bool
+    
+    private (set) var isEditing: Bool
+    
+    private (set) var currentError: Error?
+    
+    
+    init() {
+        currentUser = Auth.auth().currentUser
+        isAuthenticating = false
+        isEditing = false
+    }
+    
+    mutating func register(withEmail email: String, password: String) async {
+        do {
+            isAuthenticating = true
+            currentError = nil
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            currentUser = result.user
+            isAuthenticating = false
+        } catch {
+            currentError = error
+            isAuthenticating = false
+        }
+    }
+    
+    mutating func login(withEmail email: String, password: String) async {
+        do {
+            isAuthenticating = true
+            currentError = nil
+            let result = try await Auth.auth().signIn(withEmail: email, password: password)
+            currentUser = result.user
+            isAuthenticating = false
+        } catch {
+            currentError = error
+            isAuthenticating = false
+        }
+    }
+    
+    mutating func logout() {
+        do {
+            currentError = nil
+            try Auth.auth().signOut()
+            currentUser = Auth.auth().currentUser
+        } catch {
+            currentError = error
+        }
+    }
+    
+    mutating func deleteAccount() async {
+        do {
+            currentError = nil
+            isEditing = true
+            try await currentUser?.delete()
+            currentUser = Auth.auth().currentUser
+            isEditing = false
+        } catch {
+            currentError = error
+            isEditing = false
+            currentUser = Auth.auth().currentUser
+        }
+    }
+    
+    mutating func updateEmail(to email: String) async {
+        do {
+            currentError = nil
+            isEditing = true
+            try await currentUser?.sendEmailVerification(beforeUpdatingEmail: email)
+            currentUser = Auth.auth().currentUser
+            isEditing = false
+        } catch {
+            currentError = error
+            isEditing = false
+            currentUser = Auth.auth().currentUser
+        }
+    }
+    
+    mutating func updateName(to name: String) async {
+        do {
+            currentError = nil
+            isEditing = true
+            let changeRequest = currentUser?.createProfileChangeRequest()
+            changeRequest?.displayName = name
+            try await changeRequest?.commitChanges()
+            currentUser = Auth.auth().currentUser
+            isEditing = false
+        } catch {
+            currentError = error
+            isEditing = false
+            currentUser = Auth.auth().currentUser
+        }
+    }
+    
+    mutating func clearError() {
+        currentError = nil
+    }
+}
