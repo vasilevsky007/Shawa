f86b4d258aee04249d74c5d76b48b944383a0756
alex <alex.vasilevski007@gmail.com>
2024-05-05 08:11:52 +0300

    feat: implemented received orders page

---
+++ Shawa/View/Components/Shared/SegmentedProgressBar.swift
@@ -0,0 +1,18 @@
+//
+//  SegmentedProgressBar.swift
+//  Shawa
+//
+//  Created by Alex on 4.05.24.
+//
+
+import SwiftUI
+
+struct SegmentedProgressBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SegmentedProgressBar()
+}
